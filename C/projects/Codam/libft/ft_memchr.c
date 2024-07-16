02391cab638ac098c912468b2ad73bd65e56d0fe
a0f <a0f@github.com>
2024-07-16 14:37:40 +0200

    adding memchr and memcmp

---
+++ C/projects/Codam/libft/ft_memchr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: a0f <a0f@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 13:34:48 by a0f               #+#    #+#             */
+/*   Updated: 2024/07/16 13:55:01 by a0f              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+#include <stdio.h>
+
+void *ft_memchr(const void *s, int c, size_t n)
+{
+    unsigned char *ptr;
+
+    ptr = (unsigned char *)s;
+    while (n > 0)
+    {
+        if (*ptr == (unsigned char)c)
+        {
+            return (ptr);
+        }
+        ptr++;
+        n--;
+    }
+    return (NULL);
+}
+
+int main(void)
+{
+    char s1[] = "Hey, find this x";
+    
+    printf("Found string: %s\n", (char *)(ft_memchr(s1, 'x', 16)));    
+}
+
---
+++ C/projects/Codam/libft/ft_memcmp.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: a0f <a0f@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 14:12:16 by a0f               #+#    #+#             */
+/*   Updated: 2024/07/16 14:20:38 by a0f              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+#include <stdio.h>
+#include <string.h>
+
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+    unsigned char *ptr1;
+    unsigned char *ptr2;
+
+    ptr1 = (unsigned char*)s1;
+    ptr2 = (unsigned char*)s2;
+
+    while (n > 0)
+    {
+        if (*ptr1 != *ptr2)
+        {
+            return (*ptr1 - *ptr2);
+        }
+        ptr1++;
+        ptr2++;
+        n--;
+    }
+    return (0);
+}
+
+int main(void)
+{
+    char s1[] = "Hello";
+    char s2[] = "HelloWorld";
+    
+
+    printf("Mine: %i\n", ft_memcmp(s1, s2, 10));
+    printf("strncmp: %i\n", memcmp(s1, s2, 10));
+}
